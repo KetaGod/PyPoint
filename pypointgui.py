1362df1b28889e0162d418000f3f7325ffb02d3b
KetaGod <ketagod666@proton.me>
2023-10-25 15:22:52 -0700

    pypointgui.py

--- pypointgui.py
+++ pypointgui.py
@@ -1,12 +1,21 @@
+### Program Coded/Created by KetaGod | keta666 on Discord ###
+### For Questions, Comments, or Requests. Please msg me   ###
+### via Discord or my email @ ketagod666@proton.me        ###
+
 import tkinter as tk
 from tkinter import filedialog
 from pptx import Presentation
 from docx import Document
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.text_rank import TextRankSummarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
 import subprocess
 import sys
 import random
 
-def extract_text_from_pptx(pptx_file, word_output_file, summary_output_file):
+def extract_text_from_pptx(pptx_file, word_output_file):
     presentation = Presentation(pptx_file)
     
     all_text = ""
@@ -19,6 +28,45 @@ def extract_text_from_pptx(pptx_file, word_output_file, summary_output_file):
     doc = Document()
     doc.add_paragraph(all_text)
     doc.save(word_output_file)
+
+def summarize_text_with_length(text, summary_length=3):
+    # Initialize TextRankSummarizer with specific language
+    summarizer = TextRankSummarizer(Stemmer("english"))
+
+    # Set summarization ratio based upon summary length
+    summarizer.stop_words = get_stop_words("english")
+    summarized_sentences = summarizer(PlaintextParser.from_string(text, Tokenizer("english")))
+
+    # Select specified number of sentences for summary
+    num_sentences = min (len(summarized_sentences), summary_length)
+    summary = " ".join(str(sentence) for sentence in summarized_sentences[:num_sentences])
+
+    return summary
+
+def summarize_pptx_with_length():
+    pptx_file = pptx_entry.get()
+    word_output_file = word_output_entry.get()
+    
+    presentation = Presentation(pptx_file)
+    
+    all_text = ""
+    for slide in presentation.slides:
+        for shape in slide.shapes:
+            if hasattr(shape, "text"):
+                all_text += shape.text + "\n"
+    
+    # Prompt the user for the summary length
+    summary_length = int(input("Enter the desired summary length (number of sentences): "))
+    
+    # Summarize the text with the specified length
+    summarized_text = summarize_text_with_length(all_text, summary_length)
+    
+    # Save the summary to a Word document
+    doc = Document()
+    doc.add_paragraph(summarized_text)
+    doc.save(word_output_file)
+    
+    status_label.config(text=f"Text summarized and saved to '{word_output_file}'")
 
 
 def browse_pptx():
@@ -56,6 +104,10 @@ window.configure(bg=random_color)
 
 # Apply high contrast theme for better clarity
 window.tk_setPalette(background=random_color, foreground='white')
+
+# Add a button to run text summarization with user-defined length
+summarize_with_length_button = tk.Button(window, text="Summarize Text (Custom Length)", command=summarize_pptx_with_length)
+summarize_with_length_button.grid(row=8, column=0, columnspan=3)
 
 # Create and configure GUI elements
 author_label = tk.Label(window, text="Author: KetaGod | keta666")
